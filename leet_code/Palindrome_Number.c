cbf5c44b8cfa74cc4769e209edbe8a24632050c7
SilVeR MEP <abdousami855@gmail.com>
2024-12-24 10:56:56 +0100

    11

--- leet_code/Palindrome_Number.c
+++ leet_code/Palindrome_Number.c
@@ -2,35 +2,30 @@
 
 bool isPalindrome(int x)
 {
-    int n, d = 1, c = 0;
+    int d = 1;
 
-    if( x < 0 ) return false;
-
-    while (x >= d *10)
+    while (x / d >= 10)
     {
         d *= 10;
-        c++;
-        if (x == d) return false; break;
     }
 
-    d = d / 10;
-    c = c -1 ;
+    if(x < 0 ) return false;
 
     do
-    { 
-        if(x % 10 == x / d)
-        {
-            x = (x / d) % 10;
-            d = d / 100;
-            if
-            {
-                x <=10;   
-                return true;
-                break;
-            }
-        }
-    
-    } while (x >= 10);
+    {
+        int last = x / d;
+        int first = x % 10;
 
-    return false;
+        if (first != last)
+        {
+            return false;
+        }
+
+        x = (x % d) / 10;
+        d /= 100;
+
+
+    } while (x > 0);
+
+    return true;
 }
